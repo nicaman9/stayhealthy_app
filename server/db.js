4c8d5df007851b17c37620d6062716ccca8c2535
nicaman9 <nicaman9@gmail.com>
2025-03-22 06:09:20 -0400

    updates

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:4OdoSLajUnO4vPPrRvwK9h53@172.21.117.239:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
