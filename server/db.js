83a74adf5f0f51c3deb382c9890684fafd28934e
nicaman9 <nicaman9@gmail.com>
2025-04-12 03:03:07 -0400

    updated

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:Acev6eOR1rGmuIlxL0GaOoXs@172.21.39.209:27017";
+const mongoURI =  "mongodb://root:PINQfVwKFBpTAcJ2GmPTNHrM@172.21.58.205:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 // Import necessary modules from React library
-import React, { useEffect } from 'react';
+import React from 'react';
 
 // Import components for routing from react-router-dom library
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// Import custom Navbar component
-import Navbar from './Components/Navbar/Navbar';
-
-import Landing_Page from './Components/Landing_Page/Landing_Page'
+// Import custom components
+import Notification from './Components/Notification/Notification'; // ✅ Import Notification
+import Landing_Page from './Components/Landing_Page/Landing_Page';
 import SignUp from './Components/Sign_up/Sign_up';
 import Login from './Components/Login/Login';
 import InstantConsultation from './Components/Instant_consultation/InstantConsultation';
@@ -16,26 +15,23 @@ import BookingConsultation from './Components/BookingConsultation/BookingConsult
 
 // Function component for the main App
 function App() {
-
-  // Render the main App component
   return (
     <div className="App">
-        {/* Set up BrowserRouter for routing */}
-        <BrowserRouter>
-          {/* Display the Navbar component */}
-          <Navbar/>
-
-          {/* Set up the Routes for different pages */}
+      {/* Set up BrowserRouter for routing */}
+      <BrowserRouter>
+        {/* Wrap Routes with Notification to show it on all pages */}
+        <Notification>
+          {/* Define individual Route components for different pages */}
           <Routes>
-            {/* Define individual Route components for different pages */}
-            <Route path="/" element={<Landing_Page/>}/>
-            <Route path="/SignUp" element={<SignUp/>}/>
-            <Route path="/Login" element={<Login/>}/>
-            <Route path="/InstantConsultation" element={<InstantConsultation/>}/>
-            <Route path="/FindDoctorSearch" element={<FindDoctorSearch/>}/>
+            <Route path="/" element={<Landing_Page />} />
+            <Route path="/SignUp" element={<SignUp />} />
+            <Route path="/Login" element={<Login />} />
+            <Route path="/InstantConsultation" element={<InstantConsultation />} />
+            <Route path="/FindDoctorSearch" element={<FindDoctorSearch />} />
             <Route path="/BookingConsultation" element={<BookingConsultation />} />
           </Routes>
-        </BrowserRouter>
+        </Notification>
+      </BrowserRouter>
     </div>
   );
 }
